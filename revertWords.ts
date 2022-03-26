aae98d77a15ae6b7a3fb606ece0212d697e4da2c
Boris Kuznetsov <boris.kuznetsov@latoken.com>
2022-03-26 12:51:20 +0300

    revere words solution

---
+++ revertWords.ts
@@ -0,0 +1,12 @@
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+const input: string[] = [`This is an example!`, `double  spaces`];
+
+const reverse = (input: string): string => {
+  const words: string[] = input.split(` `)
+  const reverseWords: string[] = words.map((word) => word.split(``).reverse().join(''));
+  return reverseWords.join(` `)
+}
+
+console.log(reverse(input[0]))
