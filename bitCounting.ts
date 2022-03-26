f877fb653f4300607deb31c10bcec3f26ac391f2
Boris Kuznetsov <boris.kuznetsov@latoken.com>
2022-03-26 13:12:37 +0300

    bitCounting solution

---
+++ bitCounting.ts
@@ -0,0 +1,9 @@
+// 1234 is 10011010010 ==> 5
+
+const countBits = (n: number): number => {
+  const bin = Number(n).toString(2);
+  return bin.split(``).filter(digit => Number(digit) === 1).length
+}
+
+
+console.log(countBits(1234));
